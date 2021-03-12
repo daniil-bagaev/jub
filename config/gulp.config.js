dad0bb1ab63365d3e313fe838969933cd0efca65
Багаев Даниил Анатольевич <bagaevda@skbbank.ru>
2021-03-12 10:49:32 +0500

    Commit 0.0.5

--- config/gulp.config.js
+++ config/gulp.config.js
@@ -1,4 +1,58 @@
 'use strict';
+const 
+    gulp = require('gulp'),
+    $ = require('gulp-load-plugins')({
+        debug: false,
+        pattern: ['*']
+    }),
+    rc = new Object();
+rc.path = require('./.pathrc.json');
 module.exports = {
-
+    default: {
+        charset: 'UTF-8',
+        mode: 'dev'
+    },
+    mode: {
+        dev: {
+            tasks: {
+                pug: {
+                    src: rc.path.dev.pug,
+                    dest: rc.path.build.main,
+                    plugins: [
+                        $.pug({
+                            pretty: '\t'
+                        })
+                    ]
+                },
+                less: {
+                    src: rc.path.dev.less,
+                    dest: rc.path.build.css,
+                    plugins: [
+                        $.less()
+                    ]
+                }
+            }
+        },
+        prod: {
+            tasks: {
+                css: {
+                    name: 'css'
+                }
+            }
+        },
+        dep: {
+            tasks: {
+                css: {
+                    name: 'css'
+                }
+            }
+        },
+        build: {
+            tasks: {
+                css: {
+                    name: 'css'
+                }
+            }
+        }
+    }
 };
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 const 
     gulp = require('gulp'),
     $ = require('gulp-load-plugins')({
+        debug: false,
         pattern: ['*']
     }),
     rc = new Object();
@@ -9,36 +10,85 @@ const
     rc.config = require(rc.path.config.gulp);
     rc.package = require(rc.path.config.package);
     rc.git = require(rc.path.config.git);
-gulp
-    .task('bump', () => {
-        return gulp
-            .src('./package.json')
-            .pipe($.bump({type: rc.git.patch}))
-            .pipe(gulp.dest('./'))
-    })
-gulp
-    .task('git:add', () => {
-        return gulp
-            .src('./')
-            .pipe($.git.add())
-    });
-gulp
-    .task('git:commit', () => {
-        return gulp
-            .src('./')
-            .pipe($.git.commit([rc.git.title+' '+rc.package.version, rc.git.msg], {emitData:true}))
-            .on('data', (data) => {
-                console.log(data)
-            })
-    });
-gulp
-    .task('git:push', (cb) => {
-        $.git.push('origin', 'main', (err) => {
-            if (err) 
-                throw err;
+let 
+    modes = [],
+    tasks = [];
+for (const [key, value] of Object.entries(rc.config.mode))
+    modes.push(key);
+for (const [key, val] of Object.entries(rc.config.mode[rc.config.default.mode].tasks))
+    tasks.push(key);
+const 
+    argv = require('yargs')
+        .demand(['mode','tasks'])
+        .choices('mode', modes)
+        .default('mode', rc.config.default.mode)
+        .alias('mode', 'm')
+        .describe('mode', 'Выбор среды разработки')
+        .help('help')
+        .alias('help','h')
+        .version(rc.package.version)
+        .alias('version', 'v')
+        .epilog('©' + ' ' + rc.package.year + ' ' + rc.package.author)
+        .alias('tasks', 't')
+        .describe('tasks', 'Выбор задачи для среды')
+        .default('tasks', tasks)
+        .array(tasks)
+        .argv;
+let 
+    builder = () => {
+        for (let val of tasks) 
+            tasker(rc.config.mode[argv.mode].tasks[val], argv.mode+':'+val);
+    },
+    tasker = (task, taskName) => {
+        let arrBeg = [
+                gulp.src(task.src)
+            ], 
+            arrEnd = [
+                gulp.dest(task.dest)
+            ],
+            arr = [];
+        arr = arrBeg.concat(task.plugins.concat(arrEnd));
+        gulp
+            .task(taskName, () => {
+                return $.pump(arr);
             });
-        cb();
-    });
-gulp
-    .task('git', gulp.series('bump','git:add','git:commit','git:push'));
-    
+    },
+    gitter = () => {
+        gulp
+            .task('bump', () => {
+                return gulp
+                    .src('./package.json')
+                    .pipe($.bump({type: rc.git.patch}))
+                    .pipe(gulp.dest('./'))
+            });
+        gulp
+            .task('git:add', () => {
+                return gulp
+                    .src('./')
+                    .pipe($.git.add())
+            });
+        gulp
+            .task('git:commit', () => {
+                return gulp
+                    .src('./')
+                    .pipe($.git.commit([rc.git.title+' '+rc.package.version, rc.git.msg], {emitData:true}))
+                    .on('data', (data) => {
+                        console.log(data)
+                    })
+            });
+        gulp
+            .task('git:push', (cb) => {
+                $.git.push('origin', 'main', (err) => {
+                    if (err) 
+                        throw err;
+                    });
+                cb();
+            });
+        gulp
+            .task('git', gulp.series('bump','git:add','git:commit','git:push'));
+    },
+    runner = () => {
+
+    };
+//builder();
+gitter();
