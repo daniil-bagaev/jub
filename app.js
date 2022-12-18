04c43029c8667d7e15f2c851ad8762792c751665
Daniil Bagaev <daniil.bagaev@yahoo.com>
2022-12-18 18:58:56 +0500

    v.0.0.2
    Add new features

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const jub = require('../jubatus/index.js');
 const jubat = require('jubat');
 
 //console.log('TEST LOG:');
-//jub.log('hello', {color: '#ff0000', style: 'underline'});
+//jubat.log('hello', {color: '#ff0000', style: 'underline'});
 //console.log('TEST SRC:');
-//console.log(jub.src('./src/**/*.pug'));
+//console.log(jubat.src('./src/**/*.pug'));
 //console.log('TEST DEST:');
-//console.log(jub.dest('./build', jub.src('./src/**/*.pug')[1]));
-jub.git()
+//console.log(jubat.dest('./build', jubat.src('./src/**/*.pug')[1]));
+jubat.git()
